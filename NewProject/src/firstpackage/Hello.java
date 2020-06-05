88fbfa0804f3f53cd170960a5fb810fa923492ff
user <user@Veena>
2020-06-05 18:34:10 +0530

    conflict change

--- NewProject/src/firstpackage/Hello.java
+++ NewProject/src/firstpackage/Hello.java
@@ -3,6 +3,9 @@ package firstpackage;
 public class Hello {
 	public static void main(String... args)
 	{
-		System.out.println("Hello");
+		System.out.println("Hello: sdafdsf");
+		System.out.println("Hello: sdafdsf");
+		System.out.println("Hello: sdafdsf");
+		System.out.println("Hello: ");
 	}
 }
